7ddd14fcd41aebabcb5ed552b73aca44bbd5a655
VeraOd <vera.odints@gmail.com>
2021-10-01 17:02:16 +0300

    add HFD data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Stool microbiome,  before and after high-fiber diet intervention
+#'
+#' The \code{data.frame} is composed by 60 genera and two variables.
+#'
+#' @format The \code{data.frame} is composed by 60 genera and 2 variables
+#' \describe{
+#'    \item{\code{genera}}{counts for 194 genera}
+#'    \item{\code{group}}{wether the sample was collected before of after the diet intervention}
+#'    \item{\code{subject_id}}{identifier of the subject}
+#'    \item{\code{sample_id}}{identifier of the sample}
+#' }
+#' @docType data
+#' @name HFD
+#' @references \url{https://www.mdpi.com/2072-6643/10/5/576/html}
+#' @keywords data
+"HFD"
